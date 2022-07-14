40b4bf97548a01974dd85219215f2aebd1eb1074
saket-rev <saketgolu6@gmail.com>
2022-07-15 01:33:10 +0530

    Procfile

--- app.js
+++ app.js
@@ -137,10 +137,11 @@ post.save(function(err){
 
 });
 
+let port = process.env.PORT;
+if (port == null || port == ""){
+  port = 3000;
+}
 
-
-
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(port, function() {
+  console.log("Server started Successfully");
 });
